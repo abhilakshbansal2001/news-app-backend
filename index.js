9215c8f93afd3a95e8c4c560da25170e017d71e2
Abhilaksh Bansal <abhilakshbansal2001@gmail.com>
2022-04-17 03:27:22 +0530

    Another commit

--- index.js
+++ index.js
@@ -31,6 +31,20 @@ app.get("/" , (req,res) => {
         })
 })
 
+app.get("/news" , (req,res) => {
+    axios.get(`https://newsdata.io/api/1/news?apikey=pub_65467390a18e52bd65f9f93a66f79968808e&language=en`)
+        .then((data) => {
+        //   setToday({...data.data});
+        // res.writeHead(200, { 'Content-Type': 'text/plain' });
+                res.send(data.data)
+        // console.log(data.data.data);
+        })
+        .catch((err) => {
+        // res.write("something went wrong");
+        res.send("Hello fucker" + err)
+        })
+})
+
 app.listen(process.env.PORT || 5000, function () {
     console.log("SERVER STARTED PORT: 5000");
 });
